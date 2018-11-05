8231501bdb850ad671716e4e18abb951410b939d
Philip Damra <djheru@gmail.com>
2018-11-04 22:00:48 -0700

    Wiring up a query and mutation resolver to prisma, using the autogenerated prisma API methods

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,7 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({data: { ...args }}, info);
+    return item;
   }
 };
 
